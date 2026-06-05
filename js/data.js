3d26637deff4c02fc13915f51a97569aa2d5b94b
ashraf hassan <38661473+ashraflx71@users.noreply.github.com>
2026-06-05 20:20:34 +0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+const matches = [
+    { team1: "البرازيل", team2: "الأرجنتين", score: "2-2", time: "20:00", group: "A", status: "مباشر" },
+    { team1: "فرنسا", team2: "ألمانيا", score: "1-0", time: "18:00", group: "B", status: "انتهت" },
+    { team1: "إنجلترا", team2: "إسبانيا", score: "", time: "22:00", group: "C", status: "قادمة" },
+    { team1: "المغرب", team2: "مصر", score: "", time: "20:30", group: "D", status: "قادمة" }
+];
